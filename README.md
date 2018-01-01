cc0253c056ecfed53b464bb9cae6028578c2cbfa
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-01-01 20:25:25 +0100

    Quizlet authentication flow: happy case.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Quizler backend
 
 ```
-$ pip install Flask
-$ CLIENT_ID=Ab0Cd1Ef2G FLASK_APP=app.py flask run --host=0.0.0.0
+$ pip install -r requirements.txt
+$ export CLIENT_ID=Ab0Cd1Ef2G
+$ export SECRET_KEY=Ab0Cd1Ef2Gh3Ij4Kl5Mn6O
+$ FLASK_APP=app.py flask run --host=0.0.0.0
 ```
--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import os
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
+from quizler.utils import get_user_sets
 
 app = Flask(__name__)
 
+client_id = os.environ.get('CLIENT_ID')
+secret_key = os.environ.get('SECRET_KEY')
+
 @app.route("/")
 def root():
-    return render_template('start.html', client_id=os.environ.get('CLIENT_ID'))
+    code = request.args.get('code')
+    state = request.args.get('state')
+    if code and state:
+        url = 'https://api.quizlet.com/oauth/token'
+        data = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': 'http://localhost:5000/',
+        }
+        auth = requests.auth.HTTPBasicAuth(client_id, secret_key)
+        response = requests.post(url, data=data, auth=auth)
+        if response.status_code == 200:
+            user_sets = get_user_sets(client_id, response.json()['user_id'])
+            return render_template('sets.html', sets=user_sets)
+    return render_template('start.html', client_id=client_id)
