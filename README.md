b3b7b0b00b5f1ea3a7cb70fc20c8284085281fe6
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-01-01 21:39:56 +0100

    Expose 'reset' command over REST API.

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ```
 $ pip install -r requirements.txt
-$ export CLIENT_ID=Ab0Cd1Ef2G
-$ export SECRET_KEY=Ab0Cd1Ef2Gh3Ij4Kl5Mn6O
+$ export HTTP_SESSION_SECRET_KEY=Ab0Cd1Ef2Gh3Ij4Kl5Mn6Op7
+$ export QUIZLET_CLIENT_ID=Ab0Cd1Ef2G
+$ export QUIZLET_SECRET_KEY=Ab0Cd1Ef2Gh3Ij4Kl5Mn6O
 $ FLASK_APP=app.py flask run --host=0.0.0.0
 ```
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 import requests
-from flask import Flask, render_template, request
-from quizler.utils import get_user_sets
+from flask import Flask, render_template, request, session
+from quizler.utils import get_user_sets, reset_term_stats
 
 app = Flask(__name__)
 
-client_id = os.environ.get('CLIENT_ID')
-secret_key = os.environ.get('SECRET_KEY')
+app.secret_key = os.environ.get('HTTP_SESSION_SECRET_KEY')
+client_id = os.environ.get('QUIZLET_CLIENT_ID')
+secret_key = os.environ.get('QUIZLET_SECRET_KEY')
 
-@app.route("/")
+@app.route('/')
 def root():
     code = request.args.get('code')
     state = request.args.get('state')
@@ -22,6 +23,15 @@ def root():
         auth = requests.auth.HTTPBasicAuth(client_id, secret_key)
         response = requests.post(url, data=data, auth=auth)
         if response.status_code == 200:
-            user_sets = get_user_sets(client_id, response.json()['user_id'])
-            return render_template('sets.html', sets=user_sets)
+            session['user_id'] = response.json()['user_id']
+
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user_sets = get_user_sets(client_id, user_id)
+        return render_template('sets.html', sets=user_sets)
+
     return render_template('start.html', client_id=client_id)
+
+@app.route('/api/1.0/reset/<setname>/<term>', methods=['PUT'])
+def reset(setname, term):
+    reset_term_stats(setname, term, client_id, session['user_id'])
